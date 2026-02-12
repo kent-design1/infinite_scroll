62a6aa4f4087097df990912449caed0cf55c13de
Asamoah Bamfo Israel Kenneth <waasamoa@n0008125.home>
2026-02-12 20:49:52 +0100

    changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,24 +9,24 @@ import "./globals.css";
 const dmSans = DM_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-    title: "Anime Vault",
-    description: "Your favorite anime, all in one place.",
+  title: "Anime Vault",
+  description: "Your favorite anime, all in one place.",
 };
 
 export default function RootLayout({
-                                       children,
-                                   }: {
-    children: React.ReactNode;
+  children,
+}: {
+  children: React.ReactNode;
 }) {
-    return (
-        <html lang="en">
-        <body className={dmSans.className}>
+  return (
+    <html lang="en">
+      <body className={dmSans.className}>
         <main className="max-w-7xl mx-auto bg-[#0F1117]">
-            <Hero />
-            {children}
-            <Footer />
+          <Hero />
+          {children}
+          <Footer />
         </main>
-        </body>
-        </html>
-    );
+      </body>
+    </html>
+  );
 }
